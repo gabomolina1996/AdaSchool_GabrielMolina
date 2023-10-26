c8c58a1f30a7e23ccb4d9f876d4ec67719604f17
gabomolina1996 <148495066+gabomolina1996@users.noreply.github.com>
2023-10-26 14:33:41 -0600

    Create archivo.py

--- Variables_Python/archivo.py
+++ Variables_Python/archivo.py
@@ -1,6 +1,9 @@
-entero = 10
-booleano = False
-striger = "Cadena de Texto"
-flotante = 1.5
-concatenado = str(entero), str(flotante), str(booleano), striger
-print(concatenado)
+entero = 100 #Variable tipo INT. Puede contener un gran número limitado únicamente por la memoria de la PC
+booleano = False #Variable tipo Boolean
+striger = "Cadena de Texto" #Variable tipo String.
+flotante = 1.5 #Variable tipo float. Puede contener valores entre 2.2250738585072014e-308 & 1.7976931348623157e+308
+concatenado = str(entero), str(flotante), str(booleano), striger #Proceso de concatenación
+suma_pares=(entero/2)*((entero/2)+1) #Formula ligeramente simplificada para determinar la suma de los primeros pares de un número entero (incluyendolo).
+print(concatenado) #Muestra la concatenación
+print(suma_pares) #Muestra el resultado de la suma de los primeros números pares del entero definido.
+
